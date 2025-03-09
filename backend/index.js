dc9d0de63e7a11d228e04b0fa082b7a1741ba34b
Aviral3501 <aviralsingh3501@gmail.com>
2025-03-09 15:37:41 +0530

    basic routes + mongoose schema for user

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ import path from "path";
 
 import { connectDB } from "./db/connectDB.js";
 
-// import authRoutes from "./routes/auth.route.js";
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
@@ -19,15 +19,15 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json()); // allows us to parse incoming requests:req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
 
-// app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+// 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
+// 	app.get("*", (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+// 	});
+// }
 
 app.listen(PORT, () => {
 	connectDB();
